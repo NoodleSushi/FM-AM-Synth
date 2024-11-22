c1f8d1d60fa92048e5c8ae3ceca2b8164ecc554d
NoodleSushi <ytmikesstuffs@gmail.com>
2024-11-22 23:02:53 +0800

    Add Web MIDI support check in useMidi hook for improved error handling

--- src/Hooks/useMidi.ts
+++ src/Hooks/useMidi.ts
@@ -2,22 +2,26 @@ import { useEffect } from 'react'
 
 function useMidi(onNoteOn?: (note: number) => void, onNoteOff?: (note: number) => void) {
   useEffect(() => {
-    navigator.requestMIDIAccess().then((midiAccess) => {
-      for (const input of midiAccess.inputs.values()) {
-        input.onmidimessage = (e) => {
-          if (e.data) {
-            const [command, note] = e.data
-            if (command === 144) {
-              if (onNoteOn) onNoteOn(note)
-            } else if (command === 128) {
-              if (onNoteOff) onNoteOff(note)
+    if (navigator.requestMIDIAccess) {
+      navigator.requestMIDIAccess().then((midiAccess) => {
+        for (const input of midiAccess.inputs.values()) {
+          input.onmidimessage = (e) => {
+            if (e.data) {
+              const [command, note] = e.data
+              if (command === 144) {
+                if (onNoteOn) onNoteOn(note)
+              } else if (command === 128) {
+                if (onNoteOff) onNoteOff(note)
+              }
             }
           }
         }
-      }
-    }).catch((err) => {
-      console.error('Failed to get MIDI access', err);
-    })
+      }).catch((err) => {
+        console.error('Failed to get MIDI access', err);
+      })
+    } else {
+      console.error('Web MIDI not supported');
+    }
   }, [])
 }
 
